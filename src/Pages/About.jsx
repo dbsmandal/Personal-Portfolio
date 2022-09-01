f0fe5f5569d77a8f6dcd6ff7075b390e7bb14135
dbsmandal <dipakmandaljio420@gmail.com>
2022-09-02 01:36:41 +0530

    About Section Done With Help of props

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import HeroSection from '../Component/HeroSection'
 
 const About = () => {
+  const data={
+    name:"Dipak Mandal",
+    image:"./images/about1.svg"
+  }
   return (
     <div>
-      About
+     <HeroSection {...data}/>
     </div>
   )
 }
 
 export default About
+ 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import HeroSection from '../Component/HeroSection'
 
 const Home = () => {
+  const data={
+    name:"Dbsmandal Portfolio",
+    image:"./images/hero.svg"
+  }
   return (
     <div>
-      <HeroSection/>
+      <HeroSection {...data}/>
     </div>
   )
 }
 
 export default Home 
+ 
